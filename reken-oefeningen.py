d3c50a03003a165f2aa15411b754371ccca3d09a
RowanPl <Rowanplooij@hotmail.com>
2024-05-13 13:39:03 +0200

    update

---
+++ reken-oefeningen.py
@@ -0,0 +1,28 @@
+### Laat python 2 getallen optellen en het resultaat printen
+#  5 + 3 = 8
+
+
+
+### Laat python 2 getallen vermenigvuldigen en het resultaat printen
+#  3 * 4 = 12
+
+
+### Geef nu het resultaat weer in een string. waar de getallen en het resultaat in staan.
+
+
+### Laat python de wortel van een getal berekenen en het resultaat printen
+# De wortel van 16 is 4
+# Tip gebruik ** om de wortel te berekenen
+
+### Laat python de rest van een deling berekenen en het resultaat printen
+#  De rest van 10 / 3 is 1
+
+
+### Maak een calculator die 2 getallen optelt, aftrekt, vermenigvuldigd en deelt
+# Gebruik hiervoor input om de getallen te vragen
+# Voer getal 1 in: 5
+# Voer getal 2 in: 3
+# 5 + 3 = 8
+# 5 - 3 = 2
+# 5 * 3 = 15
+# 5 / 3 = 1.6666666666666667
---
+++ vergelijking-oefeningen.py
@@ -0,0 +1,18 @@
+# geef 2 getallen op en kijk of ze gelijk zijn
+# 5 en 5 zijn gelijk
+# 5 en 3 zijn niet gelijk
+
+
+# geef 2 getallen op en kijk of getal1 groter is dan getal2
+# 5 is groter dan 3
+# 3 is niet groter dan 5
+
+
+# geef 2 getallen op en kijk of getal1 kleiner is dan getal2 of gelijk aan getal2
+# 5 is kleiner dan 3
+# 3 is kleiner dan 5
+# 5 is niet kleiner dan 5
+
+# Kijk of een string gelijk is aan een andere string
+# John is gelijk aan John
+# John is niet gelijk aan Doeg
