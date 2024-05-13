03d4c1fa47fbeb2e2e36e626e4ea7a66b7363726
RowanPl <Rowanplooij@hotmail.com>
2024-05-13 13:39:52 +0200

    uitwerkingen

--- reken-oefeningen.py
+++ reken-oefeningen.py
@@ -1,28 +1,51 @@
-### Laat python 2 getallen optellen en het resultaat printen
+# Laat python 2 getallen optellen en het resultaat printen
 #  5 + 3 = 8
 
+getal1 = 5
+getal2 = 3
+resultaat = getal1 + getal2
+print(resultaat)
 
-
-### Laat python 2 getallen vermenigvuldigen en het resultaat printen
+# Laat python 2 getallen vermenigvuldigen en het resultaat printen
 #  3 * 4 = 12
+getal2 = 3
+getal3 = 4
+resultaat = getal2 * getal3
+print(resultaat)
 
-
-### Geef nu het resultaat weer in een string. waar de getallen en het resultaat in staan.
-
+# Geef nu het resultaat weer in een string. waar de getallen en het resultaat in staan.
+print(f"{getal2} * {getal3} = {resultaat}")
 
 ### Laat python de wortel van een getal berekenen en het resultaat printen
-# De wortel van 16 is 4
+## De wortel van 16 is 4
 # Tip gebruik ** om de wortel te berekenen
+getal4 = 16
+wortel = getal4 ** 0.5
+print(f"De wortel van {getal4} is {wortel}")
 
 ### Laat python de rest van een deling berekenen en het resultaat printen
 #  De rest van 10 / 3 is 1
-
+getal5 = 10
+getal6 = 3
+rest = getal5 % getal6
+print(f"De rest van {getal5} / {getal6} is {rest}")
 
 ### Maak een calculator die 2 getallen optelt, aftrekt, vermenigvuldigd en deelt
-# Gebruik hiervoor input om de getallen te vragen
+## Gebruik hiervoor input om de getallen te vragen
 # Voer getal 1 in: 5
 # Voer getal 2 in: 3
 # 5 + 3 = 8
 # 5 - 3 = 2
 # 5 * 3 = 15
 # 5 / 3 = 1.6666666666666667
+
+getal7 = int(input("Voer getal 1 in: "))
+getal8 = int(input("Voer getal 2 in: "))
+resultaat = getal7 + getal8
+print(f"{getal7} + {getal8} = {resultaat}")
+resultaat = getal7 - getal8
+print(f"{getal7} - {getal8} = {resultaat}")
+resultaat = getal7 * getal8
+print(f"{getal7} * {getal8} = {resultaat}")
+resultaat = getal7 / getal8
+print(f"{getal7} / {getal8} = {resultaat}")
--- vergelijking-oefeningen.py
+++ vergelijking-oefeningen.py
@@ -2,17 +2,32 @@
 # 5 en 5 zijn gelijk
 # 5 en 3 zijn niet gelijk
 
+getal1 = 5
+getal2 = 5
+getal3 = 3
+print(f"zijn {getal1} en {getal2} gelijk? :  {getal2 == getal1}")
+print(f"zijn {getal1} en {getal3} gelijk? :  {getal3 == getal1}")
 
 # geef 2 getallen op en kijk of getal1 groter is dan getal2
 # 5 is groter dan 3
 # 3 is niet groter dan 5
+print(f"{getal1} is groter dan {getal3} : {getal1 > getal3}")
+print(f"{getal3} is groter dan {getal1} : {getal3 > getal1}")
 
 
 # geef 2 getallen op en kijk of getal1 kleiner is dan getal2 of gelijk aan getal2
 # 5 is kleiner dan 3
 # 3 is kleiner dan 5
 # 5 is niet kleiner dan 5
+print(f"{getal1} is kleiner dan of gelijk aan {getal3} : {getal1 < getal3}")
+print(f"{getal3} is kleiner dan of gelijk aan {getal1} : {getal3 < getal1}")
+print(f"{getal1} is kleiner dan of gelijk aan {getal2} : {getal1 <= getal2}")
 
 # Kijk of een string gelijk is aan een andere string
 # John is gelijk aan John
-# John is niet gelijk aan Doeg
+# John is niet gelijk aan Doe
+name = "John"
+test = name
+print(name is test)
+print(name == "John")
+print(name == "Doe")
