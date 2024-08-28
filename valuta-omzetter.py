b9cd5361032882c6ff774fc09c2c933603dc0dfa
Elwyn <elwyn_de_neve@hotmail.com>
2024-08-28 15:17:22 +0200

    opdracht valuta omzetter toegevoegd

---
+++ valuta-omzetter.py
@@ -0,0 +1,20 @@
+# Opdracht: Valuta Omzetter
+# Doel: Maak een programma dat de gebruiker vraagt om een bedrag in euro's
+# en de doelvaluta, waarna het programma het bedrag converteert naar de doelvaluta
+# en het resultaat toont.
+
+# Benodigdheden:
+
+# Gebruik van input() om invoer van de gebruiker te verzamelen.
+# Omzetten van strings naar numerieke waarden voor berekeningen.
+# Gebruik van print statements om resultaten te tonen.
+
+
+# Stappen:
+
+# Vraag de gebruiker om een bedrag in euro's in te voeren.
+# Vraag de gebruiker om de doelvaluta te kiezen (bijvoorbeeld USD, GBP, of JPY).
+# Gebruik een vooraf gedefinieerde set van wisselkoersen om de conversie uit te voeren.
+# Zoek zelf online naar actuele wisselkoersen.
+# Bereken het geconverteerde bedrag.
+# Toon het originele bedrag en het geconverteerde bedrag in de doelvaluta.
